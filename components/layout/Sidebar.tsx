2b244a007e9367bc4a1306fc9ff2d8ec61b13a0b
shohan-pherones <shohan.sub.56@gmail.com>
2023-03-28 21:47:09 +0600

    layout added

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,44 @@
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import SidebarItem from "./SidebarItem";
+import SidebarLogo from "./SidebarLogo";
+
+const Sidebar = () => {
+  const items = [
+    {
+      label: "Home",
+      href: "/",
+      icon: BsHouseFill,
+    },
+    {
+      label: "Notifications",
+      href: "/notifications",
+      icon: BsBellFill,
+    },
+    {
+      label: "Profile",
+      href: "/users/123",
+      icon: FaUser,
+    },
+  ];
+
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ components/layout/SidebarItem.tsx
@@ -0,0 +1,29 @@
+import { IconType } from "react-icons";
+
+interface SidebarItemProps {
+  label: string;
+  href?: string;
+  icon: IconType;
+  onClick?: () => void;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  label,
+  href,
+  icon: Icon,
+  onClick,
+}) => {
+  return (
+    <div className="flex flex-row items-center">
+      <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300/10 cursor-pointer lg:hidden">
+        <Icon size={28} color="white" />
+      </div>
+      <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-300/10 cursor-pointer">
+        <Icon size={24} color="white" />
+        <p className="hidden lg:block text-white text-xl">{label}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarItem;
