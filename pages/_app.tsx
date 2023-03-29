a3ce14d78373b5a461bc912be43902daae716994
shohan-pherones <shohan.sub.56@gmail.com>
2023-03-29 14:32:59 +0600

    modal added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import Layout from "@/components/Layout";
+import Modal from "@/components/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Modal isOpen title="Test Modal" actionLabel="Submit" />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
