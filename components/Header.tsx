3b8030759c4a31bbd8926f6d69f6bb40e5adc853
shohan-pherones <shohan.sub.56@gmail.com>
2023-03-29 13:41:38 +0600

    header added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,34 @@
 import { useRouter } from "next/router";
 import { useCallback } from "react";
+import { BiArrowBack } from "react-icons/bi";
 
-const Header = () => {
+interface HeaderProps {
+  label: string;
+  showBackArrow?: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({ label, showBackArrow }) => {
   const router = useRouter();
 
   const handleBack = useCallback(() => {
     router.back();
   }, [router]);
 
-  return <div></div>;
+  return (
+    <div className="border-b-[1px] border-neutral-800 p-5">
+      <div className="flex fle-row items-center gap-2">
+        {showBackArrow && (
+          <BiArrowBack
+            onClick={handleBack}
+            size={20}
+            color="white"
+            className="cursor-pointer hover:opacity-70 transition"
+          />
+        )}
+        <h1 className="text-white text-xl font-semibold">{label}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
