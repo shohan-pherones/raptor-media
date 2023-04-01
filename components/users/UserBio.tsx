fa846d6a973d39490f441e9284a46d006a9a4387
shohan-pherones <shohan.sub.56@gmail.com>
2023-04-01 13:47:17 +0600

    user bio added

---
+++ components/users/UserBio.tsx
@@ -0,0 +1,62 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useUser from "@/hooks/useUser";
+import { format } from "date-fns";
+import { useMemo } from "react";
+import Button from "../Button";
+import { BiCalendar } from "react-icons/bi";
+
+interface UserBioProps {
+  userId: string;
+}
+
+const UserBio: React.FC<UserBioProps> = ({ userId }) => {
+  const { data: currentUser } = useCurrentUser();
+  const { data: fetchedUser } = useUser(userId);
+
+  const createdAt = useMemo(() => {
+    if (!fetchedUser?.createdAt) {
+      return null;
+    }
+
+    return format(new Date(fetchedUser.createdAt), "MMMM yyyy");
+  }, [fetchedUser?.createdAt]);
+
+  return (
+    <div className="border-b-[1px] border-neutral-800 pb-4">
+      <div className="flex justify-end p-2">
+        {currentUser?.id === userId ? (
+          <Button secondary label="Edit" onClick={() => {}} />
+        ) : (
+          <Button onClick={() => {}} label={"Follow"} secondary />
+        )}
+      </div>
+
+      <div className="mt-8 px-4">
+        <div className="flex flex-col">
+          <p className="text-white text-2xl font-semibold">
+            {fetchedUser?.name}
+          </p>
+          <p className="text-md text-neutral-500">@{fetchedUser?.username}</p>
+        </div>
+        <div className="flex flex-col mt-4">
+          <p className="text-white">{fetchedUser?.bio}</p>
+          <div
+            className="
+              flex 
+              flex-row 
+              items-center 
+              gap-2 
+              mt-4 
+              text-neutral-500
+          "
+          >
+            <BiCalendar size={24} />
+            <p>Joined {createdAt}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserBio;
---
+++ pages/users/[userId].tsx
@@ -0,0 +1,30 @@
+import Header from "@/components/Header";
+import UserBio from "@/components/users/UserBio";
+import UserHero from "@/components/users/UserHero";
+import useUser from "@/hooks/useUser";
+import { useRouter } from "next/router";
+import { ClipLoader } from "react-spinners";
+
+const UserView = () => {
+  const router = useRouter();
+  const { userId } = router.query;
+  const { data: fetchedUser, isLoading } = useUser(userId as string);
+
+  if (isLoading || !fetchedUser) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <ClipLoader color="lightblue" size={40} />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Header showBackArrow label={fetchedUser?.name} />
+      <UserHero userId={userId as string} />
+      <UserBio userId={userId as string} />
+    </>
+  );
+};
+
+export default UserView;
