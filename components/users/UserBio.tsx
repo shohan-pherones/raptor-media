f742053633c0cefd3a7693a5aeab45667229639a
shohan-pherones <shohan.sub.56@gmail.com>
2023-04-01 15:05:04 +0600

    user profile ui added

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -38,20 +38,23 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
           </p>
           <p className="text-md text-neutral-500">@{fetchedUser?.username}</p>
         </div>
+
         <div className="flex flex-col mt-4">
           <p className="text-white">{fetchedUser?.bio}</p>
-          <div
-            className="
-              flex 
-              flex-row 
-              items-center 
-              gap-2 
-              mt-4 
-              text-neutral-500
-          "
-          >
+          <div className="flex flex-row items-center gap-2 mt-4  text-neutral-500">
             <BiCalendar size={24} />
             <p>Joined {createdAt}</p>
+          </div>
+        </div>
+
+        <div className="flex flex-row items-center mt-4 gap-6">
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">{fetchedUser?.followingIds?.length}</p>
+            <p className="text-neutral-500">Following</p>
+          </div>
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">{fetchedUser?.followersCount || 0}</p>
+            <p className="text-neutral-500">Followers</p>
           </div>
         </div>
       </div>
