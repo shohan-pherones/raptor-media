b80d25c4502f5aac29a525140b91acd2860c7cf8
shohan-pherones <shohan.sub.56@gmail.com>
2023-04-02 14:39:56 +0600

    posts added

---
+++ components/posts/PostFeed.tsx
@@ -0,0 +1,20 @@
+import usePosts from "@/hooks/usePosts";
+import PostItem from "./PostItem";
+
+interface PostFeedProps {
+  userId?: string;
+}
+
+const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
+  const { data: posts = [] } = usePosts(userId);
+
+  return (
+    <>
+      {posts.map((post: Record<string, any>) => (
+        <PostItem userId={userId} key={post.id} data={post} />
+      ))}
+    </>
+  );
+};
+
+export default PostFeed;
--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import PostFeed from "@/components/posts/PostFeed";
 import UserBio from "@/components/users/UserBio";
 import UserHero from "@/components/users/UserHero";
 import useUser from "@/hooks/useUser";
@@ -23,6 +24,7 @@ const UserView = () => {
       <Header showBackArrow label={fetchedUser?.name} />
       <UserHero userId={userId as string} />
       <UserBio userId={userId as string} />
+      <PostFeed userId={userId as string} />
     </>
   );
 };
