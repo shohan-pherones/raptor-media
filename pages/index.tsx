913a6d96aff6a55e17a2391247b934967c969d4c
shohan-pherones <shohan.sub.56@gmail.com>
2023-04-02 14:27:10 +0600

    posting form added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Form from "@/components/Form";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
       <Header label="Home" />
+      <Form placeholder="What's on your mind?" />
     </>
   );
 }
