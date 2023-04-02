ca91e0fc5532fc2e29d91339f771126a24535dbb
shohan-pherones <shohan.sub.56@gmail.com>
2023-04-03 03:54:28 +0600

    notification done

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -21,6 +21,7 @@ const Sidebar = () => {
       href: "/notifications",
       icon: BsBellFill,
       auth: true,
+      alert: currentUser?.hasNotification,
     },
     {
       label: "Profile",
@@ -42,6 +43,7 @@ const Sidebar = () => {
               label={item.label}
               icon={item.icon}
               auth={item.auth}
+              alert={item.alert}
             />
           ))}
           {currentUser && (
--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -3,6 +3,7 @@ import { IconType } from "react-icons";
 import { useCallback } from "react";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useLoginModal from "@/hooks/useLoginModal";
+import { BsDot } from "react-icons/bs";
 
 interface SidebarItemProps {
   label: string;
@@ -10,6 +11,7 @@ interface SidebarItemProps {
   icon: IconType;
   onClick?: () => void;
   auth?: boolean;
+  alert?: boolean;
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
@@ -18,6 +20,7 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
   icon: Icon,
   onClick,
   auth,
+  alert,
 }) => {
   const router = useRouter();
   const { data: currentUser } = useCurrentUser();
@@ -39,10 +42,16 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
     <div onClick={handleClick} className="flex flex-row items-center">
       <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300/10 cursor-pointer lg:hidden">
         <Icon size={28} color="white" />
+        {alert ? (
+          <BsDot className="text-sky-500 absolute -top-4 left-0" size={70} />
+        ) : null}
       </div>
       <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-300/10 cursor-pointer">
         <Icon size={24} color="white" />
         <p className="hidden lg:block text-white text-xl">{label}</p>
+        {alert ? (
+          <BsDot className="text-sky-500 absolute -top-4 left-0" size={70} />
+        ) : null}
       </div>
     </div>
   );
